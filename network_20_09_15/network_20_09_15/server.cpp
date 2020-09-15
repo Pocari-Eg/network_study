a9fc4f8890ecd3f03afb96fc0c8f5dd06667505b
Pocari-Eg <cksdud636@gmail.com>
2020-09-15 15:24:30 +0900

    20_09_15 network study

---
+++ network_20_09_15/network_20_09_15/server.cpp
@@ -0,0 +1,36 @@
+#pragma comment(lib,"ws2_32")
+#define _WINSOCK_DEPRECATRED_NO_WARNINGS
+#include<WinSock2.h>
+
+void err_quit(const char* msg) {
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&lpMsgBuf, 0, NULL);
+	MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+	
+
+}
+
+//const char* p = "socket()";
+
+int main(int argc, char* argv[]) {
+
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+	MessageBox(NULL, "윈속 초기화 성공", "알림", MB_OK);
+	
+	SOCKET tcp_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (tcp_sock == INVALID_SOCKET)err_quit("socket()");
+	MessageBox(NULL, "TCP 소켓 생성 성공", "알림", MB_OK);
+
+	closesocket(tcp_sock);
+
+	WSACleanup();
+	return 0;
+
+}
